3bd004aca815b79a865830f42035873691d3a610
jian.tang <jian.tang@dmall.com>
2021-02-07 10:37:43 +0800

    前端登录

---
+++ src/assets/rs/common.js
@@ -0,0 +1,7 @@
+/**
+ * @author     ：897863952@qq.com
+ * @date       ：Created in 2021/2/2 18:09
+ * @description：
+ * @version:
+ * @see
+ */
--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router/index'
+import antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+Vue.config.productionTip = false;
 
 Vue.use(ElementUI)
+Vue.use(antd)
 
 new Vue({
   el: '#app',
